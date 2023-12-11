872bb1ab5ffe148fd18dca5d38827d240d2f9473
Egowka <zabrodkin.eo@edu.spbstu.ru>
2023-12-11 23:02:31 +0300

    added height_search for peaks. plotter_maker for visualization. End of first iteration of project

--- Normal.py
+++ Normal.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def optimization(train):
 
@@ -6,6 +9,61 @@ def optimization(train):
         train[column] = train[column].astype(str)
         train[column] = train[column].str.replace(',', '.')
         train[column] = pd.to_numeric(train[column], errors='coerce')
-        print("!")
+        print("!") #check
 
     return train
+
+
+def heigh_search(dataframe):
+  """
+  Функция предназначена для нахождения уровня амплитуды сигнала А (ниже которой не берем – слишком маленькие капли)
+  """
+
+  positive_values = dataframe['Channel A'][dataframe['Channel A'] > 0]
+  height = np.nanquantile(positive_values, 0.15)
+
+  return height
+
+
+def plotter_maker(df, peaks_=None) -> None:
+    """
+    :param df: датасет для построения графика
+    :param peaks_: пики, если необходимо построить график для датасета в целом
+    :return: None
+    """
+
+    if peaks_ is not None:
+        plt.plot(df['Time'], df['Channel A'], label='Сигнал')
+        plt.plot(df['Time'].iloc[peaks_], df['Channel A'].iloc[peaks_], 'x', label='Исходные пики',
+                 color='red')
+        plt.legend()
+        plt.show()
+
+        pass
+
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10, 8))
+
+    # Строим графики для каждой колонки в DataFrame
+    axes[0, 0].plot(df['Time'], df['Channel A'], label='ColumnA')
+    axes[0, 1].plot(df['Time'], df['Channel B'], label='ColumnB', color='orange')
+    axes[1, 0].plot(df['Time'], df['Channel C'], label='ColumnC', color='green')
+    axes[1, 1].plot(df['Time'], df['Channel D'], label='ColumnD', color='red')
+
+    # Настроим подписи осей и легенду
+    axes[0, 0].set_title('ColumnA')
+    axes[0, 1].set_title('ColumnB')
+    axes[1, 0].set_title('ColumnC')
+    axes[1, 1].set_title('ColumnD')
+
+    # Добавим общую легенду в правом верхнем углу
+    axes[0, 0].legend(loc='upper right')
+    axes[0, 1].legend(loc='upper right')
+    axes[1, 0].legend(loc='upper right')
+    axes[1, 1].legend(loc='upper right')
+
+    # Добавим общую подпись осей y
+    fig.text(0.02, 0.5, 'Значение', va='center', rotation='vertical', fontsize=12)
+
+    # Отобразим графики
+    plt.tight_layout()
+    plt.show()
--- main.py
+++ main.py
@@ -1,38 +1,59 @@
 from scipy.signal import find_peaks
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-from Normal import optimization
+from Normal import optimization, heigh_search, plotter_maker
 
 
-def raindrop_collector(for_train, window_size: int, window_mean = None):
+def raindrop_collector(for_train, window_size: int, height_peak=None, window_mean=None, plot=True):
+    """
+
+    :param for_train: датасет для выделения капель
+    :param window_size: размер окна для ввода вручную, при отсутствии - назначается эмпирически выверенное
+    :param height_peak: высота амплитуды. При отстутствии введенных данных - берется по формуле
+    :param window_mean: начальный размер скользящего окна
+    :param plot: true - означает чертить графики в конце работы прогрммы.
+    :return: возвращает массив numpy капелек. каждая ячейка содержит df капельки
+    """
 
     if type(window_size) != int:
         raise TypeError('Тип данных должен быть int')
 
-    #hardcode для эмпирически выверенного окна
+
+    #hardcode для эмпирически выверенного окна (лучшего окна пока выведено не было) #TODO алгоритмический подбор окна
     if window_size < 50000:
         window_size = 50000
+    if height_peak is None:
+        height_peak = heigh_search(for_train)
 
 
     # находим пики с высотой больше ...
-    peaks, i = find_peaks(for_train['Channel A'], height=0.025, distance=window_size)
+    peaks, i = find_peaks(for_train['Channel A'], height=height_peak, distance=window_size)
+
+    print(str(len(peaks)) + " length of peaks\n")
 
     #храниние капель
     setup = np.empty((len(for_train),), dtype=object)
     print("!")
 
+    #поиск пиков и вырез окна
     for i, peak in enumerate(peaks):
         start = max(peak - window_size // 2, 0)
         end = min(peak + window_size // 2, len(for_train))
-
         # Вырезаем окно для каждого канала
         window = for_train.loc[start:end, ['Time', 'Channel A', 'Channel B', 'Channel C', 'Channel D']].copy()
-
         # Добавляем окно в массив капелек
         setup[i] = window
 
+    """
+    #пока что чертим 4 капельки
+    for i in range(4):
+        if plot:
+            plotter_maker(setup[i])
+    """
+    plotter_maker(for_train, peaks)
+
     return setup
+
 
 if __name__ == "__main__": #без кавычек)))
 
@@ -40,7 +61,7 @@ if __name__ == "__main__": #без кавычек)))
     df = optimization(df)
 
 #ручной ввод окна
-    WIN_SIZE = 10000
+    WIN_SIZE = 50000
     a = raindrop_collector(df, WIN_SIZE)
 
     print(a)
