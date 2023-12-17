b25d878d8a13f1a2f66d5f12431f9a557324a34b
Egowka <zabrodkin.eo@edu.spbstu.ru>
2023-12-17 17:08:52 +0300

    Добавлена функция  dt_finder для поиска расстояний между пиками

--- Normal.py
+++ Normal.py
@@ -89,19 +89,74 @@ def plotter_maker(df, peaks_=None) -> None:
     plt.show()
 
 
-def len_finder(num_mas):
+def dt_finder(dataframe):
+    #max_values = dataframe.groupby('ID').max()
+    #print(max_values.head(20))
+
+    idshniki = dataframe['ID'].unique()
+    max_values_df = pd.DataFrame(columns=dataframe.columns.difference(['Time']))
+
+    for unique_id in idshniki:
+        subset = dataframe[dataframe['ID'] == unique_id]
+        max_values_row = subset.drop('Time', axis=1).max()
+        max_values_df = pd.concat([max_values_df, max_values_row.to_frame().T], ignore_index=True)
+
+    print(max_values_df)
+
+    time_list_A = []
+    time_list_B = []
+    time_list_C = []
+    time_list_D = []
+
+    for index, row in max_values_df.iterrows():
+        time_list_A.append(dataframe.loc[dataframe['ID'] == row['ID'], 'Time'].values[0])
+        time_list_B.append(dataframe.loc[dataframe['ID'] == row['ID'], 'Time'].values[1])
+        time_list_C.append(dataframe.loc[dataframe['ID'] == row['ID'], 'Time'].values[2])
+        time_list_D.append(dataframe.loc[dataframe['ID'] == row['ID'], 'Time'].values[3])
     """
-    list = []
-    count = 1
-    for i in len(num_mas): итерируемся по капле
-        for channel in i: итерируемся по каналам капли
-            if channel != "Time" and channel != 'ID':
-                iloc[channel].max - находим пик в канале
-                list[count] = iloc['Time'](max)  - Ищем время по строке максимума из пиков
-
-            count += 1
-
-
-    :param num_mas:
-    :return:
+    dataframe['value1'] = dataframe['id'].map(lambda x: time_list_A[id_to_index[x]])
+    dataframe['value2'] = dataframe['id'].map(lambda x: list2[id_to_index[x]])
+    dataframe['value3'] = dataframe['id'].map(lambda x: list3[id_to_index[x]])
+    dataframe['value4'] = dataframe['id'].map(lambda x: list4[id_to_index[x]])
     """
+    dt1 = pd.DataFrame([a - b for a, b in zip(time_list_A, time_list_B)], columns=["dt1"])
+    dt2 = pd.DataFrame([c - d for c, d in zip(time_list_C, time_list_D)], columns = ["dt2"])
+    dt3 = pd.DataFrame([a - c for a, c in zip(time_list_A, time_list_C)], columns = ["dt3"])
+    dt4 = pd.DataFrame([b - d for b, d in zip(time_list_B, time_list_D)], columns = ["dt4"])
+
+
+    # для полного отображения дробных чисел
+    pd.set_option('display.float_format', lambda x: '%.20f' % x)
+
+
+
+    #print(dt1)
+    #print([a - b for a, b in zip(time_list_A, time_list_B)])
+
+    dataframe = pd.merge(dataframe, dt1, left_on="ID", right_index = True)
+    dataframe = pd.merge(dataframe, dt2, left_on="ID", right_index=True)
+    dataframe = pd.merge(dataframe, dt3, left_on="ID", right_index=True)
+    dataframe = pd.merge(dataframe, dt4, left_on="ID", right_index=True)
+
+    #dataframe = pd.concat([dataframe, dt1], ignore_index=True)
+    #dataframe = pd.concat([dataframe, dt2], ignore_index=True)
+    #dataframe = pd.concat([dataframe, dt3], ignore_index=True)
+    #dataframe = pd.concat([dataframe, dt4], ignore_index=True)
+
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.expand_frame_repr', False)
+    #
+
+    print(dataframe)
+
+    return dataframe
+
+
+
+
+
+
+
+
+
+
