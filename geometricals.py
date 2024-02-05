4ce258d98ff6ef9319bb32841ff88ceedaed45e2
Egowka <zabrodkin.eo@edu.spbstu.ru>
2024-02-05 20:27:57 +0300

    Без измнений

--- geometricals.py
+++ geometricals.py
@@ -5,27 +5,28 @@ from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 
 
+#channel = df['channel']
+#insert into main code:
+"""
+o(n^2) - bad bad bad. maybe loc into ID in df. if means o(1)
+for df in setup:
+    for channel in df:
+        if channel != 'time' :
+            f'semi_max_{channel[-1]}' = mean_median(channel)
+"""
+
+
+#done
 def semi_max(channel) -> Union[int, float]:
-    """
-    Функция для поиска величины полумаксимума
-    :param channel: Series с данными одной колонки с определенным ID
-    :return: Полумаксимум из channel
-    """
     return channel.max() / 2
 
 
 # time = df['time'] where df with ID (means raindrop)
 def semi_width(channel: pd.Series, time: pd.Series):
-    """
-    Поиск ширины полувысоты пика
-    :param channel: Series с данными одной колонки с определенным ID
-    :param time: Series с данными времени одной колонки с определенным ID
-    :return: Ширина полувысоты пика. При определенных условиях - разный подход
-    """
     normalized = (channel - channel.min()) / (channel.max() - channel.min())
 
-    peak_indx= np.where(normalized == 1)[0]
 
+    peak_indx= np.where(normalized == normalized.max())[0]
 
 
     if peak_indx.size == 0:
@@ -46,6 +47,15 @@ def semi_width(channel: pd.Series, time: pd.Series):
         print(left_index)
         half_width_time = (time.iloc[peak_index] - (time.iloc[left_index] +
                            time.iloc[left_index])/2) * 2
+        #plt.plot(time, normalized, label='Сигнал исходный')
+        #plt.scatter(time.iloc[left_index], normalized.iloc[left_index], c='red', marker='*', label='Слева')
+
+
+        # Добавляем легенду
+        #plt.legend()
+
+        # Показываем график
+        #plt.show()
 
         return half_width_time, left_index
 
@@ -55,7 +65,14 @@ def semi_width(channel: pd.Series, time: pd.Series):
         right_index = peak_index + indices_right[0]
         half_width_time = ((time.iloc[right_index] + time.iloc[right_index - 1])/2 -
                            time.iloc[peak_index]) * 2
+        #plt.plot(time, normalized, label='Сигнал исходный')
+        #plt.scatter(time.iloc[right_index], normalized.iloc[right_index], c='blue', marker='*', label='Справа')
 
+        # Добавляем легенду
+        #plt.legend()
+
+        # Показываем график
+        #plt.show()
         return half_width_time, right_index
 
     # Если индексы не пусты, продолжаем
@@ -71,45 +88,218 @@ def semi_width(channel: pd.Series, time: pd.Series):
         half_width_time = (time.iloc[right_index] + time.iloc[right_index - 1])/2 -\
                           (time.iloc[left_index] + time.iloc[left_index + 1]) / 2
 
-        return half_width_time, left_index
+        return half_width_time, right_index, left_index
 
     else:
         print("Не удалось найти индексы для расчета полуширины.")
         return 0
+    #return half_width_time
 
 
-def subpeak(channel, time):
+def subpeak(channel, time, peak_return=False):
+
     normalized_series = (channel - channel.min()) / (channel.max() - channel.min())
-    peak_1 = 1
+    peak_indx = np.where(normalized_series == 1)[0]
+    # Берем первый пик с высотой 1
+    if peak_indx.size == 0:
+        return 0, 0, 0, 0, 0
+
+    peak_1 = channel.max()
     #пик
     #Пики по параметрам
-    peaks, _ = find_peaks(normalized_series, height=peak_1*0.3)
 
-    peaks_1, _ = find_peaks(normalized_series, height=peak_1*0.3, distance=800)
+    peaks_2, _ = find_peaks(normalized_series, height=peak_1*0.3,
+                            prominence=(normalized_series.max()*0.2, normalized_series.max()*0.99))
 
-    peaks_2, _ = find_peaks(normalized_series, height=peak_1*0.3, prominence=(normalized_series.max()*0.3,
-                                                                              normalized_series.max()*0.99))
+    if peak_return:
+        if peaks_2.any():
+            peaks = np.full(5, -1)
+            count = 0
+            for element in peaks_2:
+                peaks[count] = element
+                count += 1
+                if count >= 5:
+                    break
+            return peaks
+        else:
+            return np.full(5, -1)
 
 
 
-    #Трешхолдовые пики
-    threshold_percent = 11
-    threshold = np.percentile(normalized_series, threshold_percent)
-    subpeaks = [peak for peak in peaks if normalized_series.iloc[peak] > threshold]
-
+    #лучший вариант - peaks_2:
     if len(peaks_2) == 0:
-        return 0
+        return 0, 0, 0, 0, 0
     elif len(peaks_2) == 1 and peaks_2[0] == normalized_series[normalized_series == peak_1].index:
-        return 0
-    else:
+        return 0, 0, 0, 0, 0
+
+
+    if peak_indx.size == 0:
+        return 0, 0, 0, 0, 0
+        # Берем первый пик с высотой 1
+    peak_index = peak_indx[0]
+
+    indices_left = np.where(normalized_series.iloc[:peak_index].values <= 0.5)[0]
+    indices_right = np.where(normalized_series.iloc[peak_index:].values <= 0.5)[0]
+
+    if indices_left.any() and indices_right.size == 0:
+
+            # если пик "упирается" справа
+        left_index = indices_left[-1]
+        print(left_index)
+        half_width_time = (time.iloc[peak_index] - (time.iloc[left_index] +
+                                                        time.iloc[left_index]) / 2) * 2
 
         p1_indx = normalized_series[normalized_series == peak_1].index
 
-        time_difference = np.abs(time.values[peaks_2] - time.values[p1_indx[0]])
+        time_difference = np.abs(time.values[peaks_2] - time.values[peak_index])
 
         time_difference = np.delete(time_difference, np.where(time_difference == 0))
-        minimum = time_difference.min()
-        return minimum
+        time_difference = np.sort(time_difference)
+        final_time = np.zeros(5)
+        count = 0
+        for element in time_difference:
+            final_time[count] = element
+            count += 1
+            if count >= 5:
+                break
+
+        return final_time[:5]
+
+
+
+
+
+    elif indices_left.size == 0 and indices_right.any():
+
+            # если пик "упирается" слева
+        right_index = peak_index + indices_right[0]
+        half_width_time = ((time.iloc[right_index] + time.iloc[right_index - 1]) / 2 -
+                               time.iloc[peak_index]) * 2
+
+
+
+        time_difference = np.abs(time.values[peaks_2] - time.values[peak_index])
+
+        time_difference = np.delete(time_difference, np.where(time_difference == 0))
+        time_difference = np.sort(time_difference)
+        final_time = np.zeros(5)
+        count = 0
+        for element in time_difference:
+            final_time[count] = element
+            count += 1
+            if count >= 5:
+                break
+
+        return final_time[:5]
+
+
+
+
+
+
+        # Если индексы не пусты, продолжаем
+    elif indices_left.any() and indices_right.any():
+            # Находим первый и последний индексы, соответствующие половинной амплитуде
+        left_index = indices_left[-1]
+        next_left = left_index + 1
+        print(left_index)
+        right_index = peak_index + indices_right[0]
+        previous_right = right_index - 1
+
+            # Рассчитываем полуширину во времени
+        half_width_time = (time.iloc[right_index] + time.iloc[right_index - 1]) / 2 - (time.iloc[left_index] + time.iloc[left_index + 1]) / 2
+
+        time_difference = np.abs(time.values[peaks_2] - time.values[peak_index])
+
+        time_difference = np.delete(time_difference, np.where(time_difference == 0))
+        time_difference = np.sort(time_difference)
+        final_time = np.zeros(5)
+        count = 0
+        for element in time_difference:
+            final_time[count] = element
+            count += 1
+            if count >= 5:
+                break
+
+        return final_time[:5]
+
+
+def semi_width_subpekas(peak, channel: pd.Series, time: pd.Series):
+    normalized = (channel - channel.min()) / (channel.max() - channel.min())
+    if peak is -1:
+        return 0, 0, 0, 0, 0
+
+    # Берем нужный сабпик
+    peak_index = peak
+
+    value = channel[peak]
+    indices_left = np.where(normalized.iloc[:peak_index].values <= value/2)[0]
+    indices_right = np.where(normalized.iloc[peak_index:].values <= value/2)[0]
+
+    if indices_left.any() and indices_right.size == 0:
+
+        # если пик "упирается" справа
+        left_index = indices_left[-1]
+        print(left_index)
+        half_width_time = (time.iloc[peak_index] - (time.iloc[left_index] +
+                                                    time.iloc[left_index]) / 2) * 2
+        # plt.plot(time, normalized, label='Сигнал исходный')
+        # plt.scatter(time.iloc[left_index], normalized.iloc[left_index], c='red', marker='*', label='Слева')
+
+        # Добавляем легенду
+        # plt.legend()
+
+        # Показываем график
+        # plt.show()
+
+        return half_width_time, left_index
+
+    elif indices_left.size == 0 and indices_right.any():
+
+        # если пик "упирается" слева
+        right_index = peak_index + indices_right[0]
+        half_width_time = ((time.iloc[right_index] + time.iloc[right_index - 1]) / 2 -
+                           time.iloc[peak_index]) * 2
+        # plt.plot(time, normalized, label='Сигнал исходный')
+        # plt.scatter(time.iloc[right_index], normalized.iloc[right_index], c='blue', marker='*', label='Справа')
+
+        # Добавляем легенду
+        # plt.legend()
+
+        # Показываем график
+        # plt.show()
+        return half_width_time, right_index
+
+    # Если индексы не пусты, продолжаем
+    elif indices_left.any() and indices_right.any():
+        # Находим первый и последний индексы, соответствующие половинной амплитуде
+        left_index = indices_left[-1]
+        next_left = left_index + 1
+        print(left_index)
+        right_index = peak_index + indices_right[0]
+        previous_right = right_index - 1
+
+        # Рассчитываем полуширину во времени
+        half_width_time = (time.iloc[right_index] + time.iloc[right_index - 1]) / 2 - \
+                          (time.iloc[left_index] + time.iloc[left_index + 1]) / 2
+
+        return half_width_time, right_index, left_index
+
+
+
+"""
+
+    plt.plot(time.values[peaks_1], normalized_series.values[peaks_1], 'x', label='Пики_1')
+
+    plt.plot(time.values[subpeaks], normalized_series.values[subpeaks],  label='Пики_3')
+    plt.legend()
+
+    """
+
+
+
+
+
 
 
 #done
@@ -150,17 +340,12 @@ def q90(channel) -> Union[int, float]:
 def k_semi_max(channel, time) -> Union[int, float]:
     semi_width_peak = semi_width(channel, time)
     if isinstance(semi_width_peak, tuple):
-        semi_width_peak = semi_width_peak[1]
-        peak = np.where(channel == channel.max())[0]
+        semi_width_peak = semi_width_peak[0]
         print(f'{semi_width_peak} полуширина пика ')
-        if semi_width_peak > peak:
-            peak = -peak
-            semi_width_peak = -semi_width_peak
-        return (channel.max() - semi_max(channel))/(peak - semi_width_peak)
+
+        return (channel.max() - semi_max(channel))/semi_width_peak
     else:
         return 0
-
-
 
 
 
